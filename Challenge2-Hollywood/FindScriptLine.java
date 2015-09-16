ca6ffe23b050a40044957f322ab2641c8e7adc4e
qilezhi2 <suyie@users.noreply.github.com>
2015-09-16 19:51:24 +0000

    Finish!

--- Challenge2-Hollywood/FindScriptLine.java
+++ Challenge2-Hollywood/FindScriptLine.java
@@ -3,12 +3,49 @@
  * A program to search for specific lines and print their line number.
  * See FindScriptLine.txt for more details
  * TODO: add your netid to the line below
- * @author put-your-netid-here
+ * @author qilezhi2
  */
 public class FindScriptLine {
 
 	public static void main(String[] args) {
 // TODO: Implement the functionality described in FindScriptLine.txt
 // TODO: Test your code manually and using the automated unit tests in FindScriptLineTest		
+		
+		boolean output = false;
+		
+		//Introduction
+		TextIO.putln("Please enter the word(s) to search for");
+		String word = TextIO.getln();
+		TextIO.putln("Searching for '"+word+"'");
+		
+		//Read the file.
+		TextIO.readFile("thematrix.txt"); 
+
+		int count = 0;
+		
+		while ( ! TextIO.eof() ) {
+			String line = TextIO.getln(); //Read the next line
+			count++; //Count the lines.
+			
+			
+			//Check all the upper and lower case, especially mix case.
+			int n = word.length();
+			String wordLower = word.toLowerCase();
+			String wordUpper = word.toUpperCase();
+			String initialLetter = String.valueOf(wordUpper.charAt(0));
+			String otherLetter = wordLower.substring(1,n);
+			String newLine = initialLetter + otherLetter;
+			if (line.indexOf(word.toLowerCase()) >=0 || line.indexOf(word.toUpperCase()) >=0 || line.indexOf(newLine) >= 0) output = true;
+			else output = false;
+			
+			//Skip all empty lines.
+			if (line.length() == 0) output = false;
+			
+			//Print the line.
+			if (output)
+				TextIO.putln(count +" - "+line.trim());
+
+		}
+		TextIO.putln("Done Searching for '"+word+"'");
 	}
 }
--- Challenge2-Hollywood/MyScriptPrinter.java
+++ Challenge2-Hollywood/MyScriptPrinter.java
@@ -14,10 +14,12 @@ public class MyScriptPrinter {
 		boolean output=false; //Set to true when we find the desired character
 		String name=""; // Only print lines for this character
 		
+		//Name?
 		TextIO.putln("Which character's lines would you like? (NEO,MORPHEUS,ORACLE)");
 		name = TextIO.getln();
 		String nameUpperCase = name.toUpperCase();
 
+		//Read the file.
 		TextIO.readFile("thematrix.txt"); // stop reading from the keyboard- use the script
 
 		//TODO: Print the name here (see ScriptPrinter.txt example output for format)
@@ -28,13 +30,14 @@ public class MyScriptPrinter {
 		// This loop will read one line at a time from the script until it
 		// reaches the end of the file and then TextIO.eof() will return true.
 		// eof means end-of-file
-		while (false == TextIO.eof()) {
+		while (! TextIO.eof()) {
 			String line = TextIO.getln(); // Read the next line
 			line = line.trim();
 			//TODO: If it's a blank line set 'output' to false	
 			//TODO: Correct the output format (see ScriptPrinter.txt example output)
 			//TODO: Re-order the three if statements so the output is correct
 			
+			//Skip all empty lines.
 			if (line.length() == 0) output = false;
 			
 			if (output) {
@@ -43,6 +46,7 @@ public class MyScriptPrinter {
 				TextIO.putln("\"");
 			}
 			
+			//This should be put in the end to avoid the line with name to be printed.
 			if (line.indexOf(nameUpperCase) >= 0)
 				output = true; // We found the character's name, time to start printing their lines
 
